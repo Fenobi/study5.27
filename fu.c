75231267194006ea15669fd125583721a9456432
Fenobi <13667739122@qq.com>
2022-05-27 23:08:23 +0800

    添加项目文件。

---
+++ fu.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+//int main()
+//{
+//	double d = 3.2;
+//	int a = d + 1;
+//	printf("%d %lf ",a, d=d+1);
+//	return 0;
+//}
+
+int main()
+{
+	int a = 12345;
+	return 0;
+}
